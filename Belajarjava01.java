6e0147696c094d44f309c7b29f78887d12b56296
dwialfarizki <dwialfaa21@gmail.com>
2024-09-19 22:41:40 +0700

    Tugas Bu Rizza

---
+++ Belajarjava01.java
@@ -0,0 +1,7 @@
+class Belajarjava01 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Dwi Alfa Rizki");
+        System.out.println("NIM : 607062400010");
+        System.out.println("Kelas D3IF - 48 - 01");
+    }
+}
